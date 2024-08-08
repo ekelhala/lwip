1223530e8d68565c879c22b9cf1a86fb521f1ec3
Emil Kelhälä <emil.kelhala@oulu.fi>
2024-08-08 15:09:41 +0300

    Add support for SNI

--- src/include/lwip/apps/mqtt.h
+++ src/include/lwip/apps/mqtt.h
@@ -86,6 +86,7 @@ struct mqtt_connect_client_info_t {
 #if LWIP_ALTCP && LWIP_ALTCP_TLS
   /** TLS configuration for secure connections */
   struct altcp_tls_config *tls_config;
+  const char* server_name;
 #endif
 };
 
